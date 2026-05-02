44f43edd41066598f5f4d37389d5d86918f36d29
NitinShelake4448 <ndshelake3@gmail.com>
2026-05-02 12:37:14 +0530

    Initial ChatSphere commit

---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,7 @@
+variable "project_name"          { type = string }
+variable "environment"           { type = string }
+variable "vpc_id"                { type = string }
+variable "public_subnet_id"      { type = string }
+variable "jenkins_instance_type" { type = string }
+variable "key_name"              { type = string }
+variable "your_ip"               { type = string }
