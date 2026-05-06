203caa373bf1fd6abaf0f144ec4e9fe70a1d24fe
NitinShelake4448 <ndshelake3@gmail.com>
2026-05-06 13:50:42 +0530

    Fix Socket.io CORS for S3

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://chatsphere-frontend-56ed6f7f.s3-website.ap-south-1.amazonaws.com",
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
@@ -15,16 +20,12 @@ export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
+const userSocketMap = {};
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
-
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
-
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
